afd44409071325e4895996480bd227da6c6176bf
faizvk <faizvk14@gmail.com>
2026-05-23 22:46:25 +0530

    refactor(backend): use logger instead of console in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
+const logger = require("./utils/logger");
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  logger.info(`Server running on http://localhost:${PORT}`);
 });
